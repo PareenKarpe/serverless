7041e9734dc7a8e6c4912b9d79618630599585b5
PareenKarpe <pareenkarpe@gmail.com>
2020-04-02 22:08:03 -0400

    commit

--- index.js
+++ index.js
@@ -1,75 +1,48 @@
-const AWS = require("aws-sdk");
-const dynamoDB = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' });
-const crypto = require("crypto");
-var ses = new AWS.SES({ region: 'us-east-1' });
+const SES = require('aws-sdk').SES;
+
 
 exports.handler = (event, context, callback) => {
-    var eParams = {
-        Key: {
-            emailId: event.Records[0].Sns.Message
-        },
-        TableName: 'dynamo'
-    };
-    dynamoDB.get(eParams, function (error, code) {
-        var jsString = JSON.stringify(code);
-        if (error) {
-            console.log("Error",error);
-        }
-        else {
-            if (Object.keys(code).length >= 0) {
-                var flag = false;
-                if(code.Item == undefined){flag = true;}else
-                    if(code.Item.timeStamp < (new Date).getTime()){flag = true;}
-                if(flag){
-                    var expirationTime = (new Date).getTime() + (60*1000*15);
-                    var params = {
-                        Item: {
-                            emailId: event.Records[0].Sns.Message,
-                            token: crypto.randomBytes(16).toString("hex"),
-                            timeStamp: expirationTime
-                        },
-                        TableName: 'dynamo'
-                    };
+  console.log(event.Records[0].Sns.Message)
+  let c = event.Records[0].Sns.Message
+  var obj = JSON.parse(c)
+  console.log("type is>>")
+  console.log(typeof obj);
+  const values = Object.values(obj)
+  console.log("here is >>")
+  console.log(values[0])
+  let Source = "bills@prod.pareenkarpe.me"
+  let ToAddresses = values[0]
 
-                    dynamoDB.put(params, function (err, data) {
-                        if (err) {
-                            callback(err, null);
-                        } else {
-                            callback(null, data);
-                            var id = params.Item.token;
-                            var username = event.Records[0].Sns.Message;
-                            console.log(username);
-                            console.log(id);
-                            var cParams = {
-                                Destination: {
-                                    ToAddresses: [username]
-                                },
-                                Message: {
-                                    Body: {
-                                        Text: {
-                                            Data: "http://"+process.env.domain+"/reset?email="+username+"&token="+id
-                                        }
-                                    },
-                                    Subject: {
-                                        Data: "Password Reset Request URL"
-                                    }
-                                },
-                                Source: "info@"+process.env.domain
-                            };
-                            ses.sendEmail(cParams, function (err, data) {
-                                if (err) {
-                                    console.log(err);
-                                }
-                                else {
-                                    console.log("EMAIL SENT");
-                                    context.succeed(event);
-                                }
-                            });
-                        }
-                    });
-                }
-            } else
-                console.log(code, "User exists");
+  if(values[2] == 'r')
+  {
+    return;
+  }
+
+  let subject = event.subject || 'Test email subject77';
+  if (typeof ToAddresses === 'string') {
+    ToAddresses = [ToAddresses];
+  }
+
+  const Charset = 'UTF-8';
+  // needs ses:SendEmail permission
+  new SES().sendEmail({
+    Destination: {ToAddresses},
+    Message: {
+      Body: {
+        Html: {
+          Charset,
+          Data: values[1]
+        },
+        Text: {
+          Charset,
+          Data: "This message is in text format7777."
         }
-    });
+      },
+      Subject: {
+        Charset,
+        Data: subject
+      }
+    },
+    Source
+  }, callback);
 };
